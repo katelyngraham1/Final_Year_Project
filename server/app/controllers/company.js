9edfbcb5436e91c9ad4de926fd5b17c0e7d42876
Katelyn <katelynxgraham@gmail.com>
2023-02-03 16:47:40 +0000

    Working on the server database and api for client

--- server/app/controllers/company.js
+++ server/app/controllers/company.js
@@ -36,27 +36,81 @@ exports.findAll = (req, res) => {
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials."
+          err.message || "Some error occurred while retrieving companies."
       });
     });
 };
 
 // Find a single Company with an id
 exports.findOne = (req, res) => {
-  
+  console.log("Select id from companies");
+  company.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    res.send(data);
+  })
+  .catch(err => {
+    res.status(500).send({
+      message:
+      err.message || "Some error occurred while retrieving one company."
+    })
+  })
 };
 
 // Update a Company by the id in the request
 exports.update = (req, res) => {
-  
+  console.log("Update company");
+  company.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    res.send(data);
+  })
+  .catch(err => {
+    res.status(500).send({
+      message:
+      err.message || "Some error occurred while updating company."
+    })
+  })
 };
 
 // Delete a Company with the specified id in the request
 exports.delete = (req, res) => {
-  
+  console.log("Delete Company with id");
+  company.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+        err.message || "Some error occurred while updating company."
+      })
+  })
 };
 
 // Delete all Companies from the database.
 exports.deleteAll = (req, res) => {
-  
+  console.log("Delete Company with id");
+  company.destroy({
+    where: {}
+  })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+        err.message || "Some error occurred while updating company."
+      })
+  })
 };
