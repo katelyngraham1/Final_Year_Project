405570e8bfb15284746a5b1ee8ea67f221e67bad
Katelyn <katelynxgraham@gmail.com>
2023-03-03 18:03:33 +0000

    Refactor Drawer Nav UI

--- client/App.js
+++ client/App.js
@@ -1,5 +1,7 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import { Image } from 'react-native';
+import { AntDesign } from "@expo/vector-icons";
 
 import Home from './screens/Home';
 import Invoices from './screens/Invoice';
@@ -13,14 +15,29 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Invoices" component={Invoices} />
-        <Drawer.Screen name="Companies" component={Companies} />
-        <Drawer.Screen name="Folders" component={Folders} />
-        <Drawer.Screen name="Settings" component={Settings} />
-        <Drawer.Screen name="Log Out" component={Logout} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home" screenOptions={{ 
+         headerShown: true,
+         headerTitle: 'File A While',
+         headerTitleStyle: { color: '#FF5733', fontSize: 30 },
+         headerRight: () => (
+          <Image
+            source={require('./assets/FileLogo.png')} 
+            style={{ width: 30, height: 30, marginRight: 15, borderRadius: 5 }} 
+          />
+        ),
+        headerTintColor: "#8626BC",
+        drawerStyle: { backgroundColor: "#C2C0C4" },
+        drawerActiveTintColor: "#8626BC",
+        drawerInactiveTintColor: "#FF5733",
+        
+      }}>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Invoices" component={Invoices} />
+      <Drawer.Screen name="Companies" component={Companies} />
+      <Drawer.Screen name="Folders" component={Folders} />
+      <Drawer.Screen name="Settings" component={Settings} />
+      <Drawer.Screen name="Log Out" component={Logout} />
+    </Drawer.Navigator>
+  </NavigationContainer>
   );
 }
