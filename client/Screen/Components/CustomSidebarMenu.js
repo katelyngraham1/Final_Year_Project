7b778f12bad864242781854330f52267b27bfe36
Katelyn <katelynxgraham@gmail.com>
2023-03-21 11:34:42 +0000

    custom sidebar/drawer component

---
+++ client/Screen/Components/CustomSidebarMenu.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import {View, Text, Alert, StyleSheet} from 'react-native';
+
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const CustomSidebarMenu = (props) => {
+  return (
+    <View style={stylesSidebar.sideMenuContainer}>
+      <View style={stylesSidebar.profileHeader}>
+        <View style={stylesSidebar.profileHeaderPicCircle}>
+          <Text style={{fontSize: 25, color: '#c381e4'}}>
+            {'File A While'.charAt(0)}
+          </Text>
+        </View>
+        <Text style={stylesSidebar.profileHeaderText}>
+          File A While
+        </Text>
+      </View>
+      <View style={stylesSidebar.profileHeaderLine} />
+
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+        <DrawerItem
+          label={({color}) => 
+            <Text style={{color: '#ff4613'}}>
+              Logout
+            </Text>
+          }
+          onPress={() => {
+            props.navigation.toggleDrawer();
+            Alert.alert(
+              'Logout',
+              'Are you sure? You want to logout?',
+              [
+                {
+                  text: 'Cancel',
+                  onPress: () => {
+                    return null;
+                  },
+                },
+                {
+                  text: 'Confirm',
+                  onPress: () => {
+                    AsyncStorage.clear();
+                    props.navigation.replace('Auth');
+                  },
+                },
+              ],
+              {cancelable: false},
+            );
+          }}
+        />
+      </DrawerContentScrollView>
+    </View>
+  );
+};
+
+export default CustomSidebarMenu;
+
+const stylesSidebar = StyleSheet.create({
+  sideMenuContainer: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#ebd5f6',
+    paddingTop: 40,
+    color: 'white',
+  },
+  profileHeader: {
+    flexDirection: 'row',
+    backgroundColor: '#ebd5f6',
+    padding: 15,
+    textAlign: 'center',
+  },
+  profileHeaderPicCircle: {
+    width: 60,
+    height: 60,
+    borderRadius: 60 / 2,
+    color: 'white',
+    backgroundColor: '#ffffff',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileHeaderText: {
+    color: 'white',
+    alignSelf: 'center',
+    paddingHorizontal: 10,
+    fontWeight: 'bold',
+  },
+  profileHeaderLine: {
+    height: 1,
+    marginHorizontal: 20,
+    backgroundColor: '#ebd5f6',
+    marginTop: 15,
+    color: '#ff4613',
+  },
+});
